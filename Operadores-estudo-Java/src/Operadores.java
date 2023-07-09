2d662f69e78000d5f3becdc2fbd5002d850af0e7
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-09 14:42:44 -0300

    modified:   .gitignore 	new file:   Operadores-estudo-Java/README.md 	new file:   Operadores-estudo-Java/src/Operadores.java

---
+++ Operadores-estudo-Java/src/Operadores.java
@@ -0,0 +1,19 @@
+// vamos iniciar o nosso estudo sobre operadores!
+
+// inicio de minha aplicação
+public class Operadores {
+    public static void main(String[] args) {
+
+        int numero = 5;
+        // numero = numero + 2;
+
+        numero = numero + 2;
+        numero++; // Quando eu falo ( numero ++), tambem estou falando que o meu valor
+        // Atribuido a uma variavel, ele e somado a mais 1,
+        // Exemplo: numero = numero + 1;
+
+        System.out.printf("O resultado e : %d%n", numero);
+        // automaticamente o meu numero ele vai ser somado a mais um
+
+    }
+}

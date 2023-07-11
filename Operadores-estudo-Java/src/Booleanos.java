8be1e89eecabe8408c55330a09024b6c2d178fae
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-10 23:53:15 -0300

    new file:   Operadores-estudo-Java/src/Booleanos.java new file:   Operadores-estudo-Java/src/BooelanosAula5.java

---
+++ Operadores-estudo-Java/src/Booleanos.java
@@ -0,0 +1,9 @@
+public class Booleanos {
+    public static void main(String[] args) {
+
+        boolean variavel = true;
+        variavel = !variavel;// aqui eu estou negando que o meu valor variavel nao e verdadeiro
+
+        System.out.println(variavel);
+    }
+}

14ac6cd54a8b99304d4fbc4c75b4e256ef0a807f
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-11 00:16:24 -0300

    Atualizaçãp no estudo Aula de operadores, AULA5

--- Operadores-estudo-Java/src/BooleanosAula5.java
+++ Operadores-estudo-Java/src/BooleanosAula5.java
@@ -1,17 +1,35 @@
 public class BooleanosAula5 {
     public static void main(String[] args) {
+        // temos aqui uma aplicação monstrando que temos o uso do if/else
         int a;
         int b;
+        int c, d;
 
         a = 10;
 
         b = 5;
+
+        c = 6;
+
+        d = 6;
 
         if (a <= b) {
             System.out.printf("Este número não e maior que %d%n: ", b);
         } else {
             System.out.println(" Número maior ");
         }
+
+        // aqui nos temos outra representação do uso de operador ternario
+        // com os sinal (?) eu digo que e positivo, e o sinal ( : ) digo quando for
+        // falso
+        String resultado = c == d ? "verdadeiro" : "falso";
+        String resultado1 = c >= d ? "resultado verdadeiro" : "resultado falso";
+        int resultado2 = a == b ? 1 : 0;
+        // o operador ternario nao se limita apenas em string's;
+
+        System.out.println(resultado);
+        System.out.println(resultado1);
+        System.out.println(resultado2);
     }
 }
 /*

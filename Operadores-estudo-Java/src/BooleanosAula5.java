e52856d97a71e47ca134d11442e969cb202a90d1
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-10 23:53:13 -0300

    Criação de dois arquivos, booleanos, if/else

---
+++ Operadores-estudo-Java/src/BooleanosAula5.java
@@ -0,0 +1,20 @@
+public class BooleanosAula5 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        a = 10;
+
+        b = 5;
+
+        if (a <= b) {
+            System.out.printf("Este número não e maior que %d%n: ", b);
+        } else {
+            System.out.println(" Número maior ");
+        }
+    }
+}
+/*
+ * ESTA APLICAÇÃO E PARA PODERMOS USAR O IF/ELSE PARA PODERMOS MOSTRAR QUAIS
+ * VALORES SÃO MAIORES
+ */

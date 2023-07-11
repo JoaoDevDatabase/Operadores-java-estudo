67eb3415324d89821c99c27265400b143acd0435
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-11 17:32:12 -0300

    Adicionado o operador equals

---
+++ Operadores-estudo-Java/src/Opeequals.java
@@ -0,0 +1,15 @@
+// arquivo Opeequals.java
+
+public class Opeequals {
+    public static void main(String[] args) {
+
+        // inicio de meu programa
+
+        String nome1 = "JOÃO";
+        String nome2 = new String("JOÃO");
+
+        System.out.println(nome1.equals(nome2));
+        // o operador equals ele vai me falar, se a string e igual ou não//
+
+    }
+}

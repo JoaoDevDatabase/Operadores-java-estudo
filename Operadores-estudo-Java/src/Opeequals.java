2755400d24fe10e24673cab03b16bd15a3598b96
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-11 17:39:46 -0300

    Atualizado, adicionado mais operadores;

--- Operadores-estudo-Java/src/Opeequals.java
+++ Operadores-estudo-Java/src/Opeequals.java
@@ -4,12 +4,17 @@ public class Opeequals {
     public static void main(String[] args) {
 
         // inicio de meu programa
-
+        // aqui fica a parte para poder acumular nos meus dados as minhas variaveis;
         String nome1 = "JOÃO";
         String nome2 = new String("JOÃO");
+        String nome3 = "RODRIGO";
+        String nome4 = "RODRIGO";
 
-        System.out.println(nome1.equals(nome2));
+        // lugar de meu output
         // o operador equals ele vai me falar, se a string e igual ou não//
+        System.out.println(nome1.equals(nome2));
+        System.out.println(nome3 == nome4);
+        // o meu operador equals ele e a mesma coisa que meu ( == );
 
     }
 }
